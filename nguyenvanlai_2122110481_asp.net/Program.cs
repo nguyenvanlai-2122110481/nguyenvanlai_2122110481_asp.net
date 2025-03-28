ade6f5cc2f3b464093819cb52f1aca4a8b0ff7cc
nguyenvanlai <nguyenvantruong04102004@gmail.com>
2025-03-28 08:51:38 +0700

    Add project files.

---
+++ nguyenvanlai_2122110481_asp.net/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Thêm dòng này để tránh lỗi:
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Đảm bảo UseAuthentication() được gọi trước UseAuthorization()
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
